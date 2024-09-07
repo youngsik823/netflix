4808413bdf8a22e9f633700ba3fb30ba24fee44d
youngsik823 <youngsik823@naver.com>
2024-09-08 00:32:14 +0900

    유튜브

--- src/pages/MovieDetail/MovieDetailPage.jsx
+++ src/pages/MovieDetail/MovieDetailPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./MovieDetailPage.style.css";
 import { useMovieDetailQuery } from "../../hooks/usePopularMovies";
@@ -7,30 +7,75 @@ import { useMovieRecommendationQuery } from "../../hooks/useMovieRecommendationQ
 import { useMovieReviewQuery } from "../../hooks/useReview.js";
 import RecommendedMovieSlide from "../Homepage/components/RecommendedMovieSlide/RecommendedMovieSlide.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar, faUser, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import {
+    faStar,
+    faUser,
+    faTriangleExclamation,
+} from "@fortawesome/free-solid-svg-icons";
 import { Button, Modal } from "react-bootstrap";
+import axios from "axios";
 
 const MovieDetailPage = () => {
     const { id } = useParams();
     const { data, isLoading, isError } = useMovieDetailQuery({ id });
     const { data: genreData } = useMovieGenreQuery();
-    const genreIds = data?.genres.map(genre => genre.id) || [];
-    const { data: recommendations, isLoading: isLoadingRecommendations, isError: isErrorRecommendations } = useMovieRecommendationQuery({ genreIds });
-    const { data: reviews, isLoading: isLoadingReviews, isError: isErrorReviews } = useMovieReviewQuery(id);
+    const genreIds = data?.genres.map((genre) => genre.id) || [];
+    const {
+        data: recommendations,
+        isLoading: isLoadingRecommendations,
+        isError: isErrorRecommendations,
+    } = useMovieRecommendationQuery({ genreIds });
+    const {
+        data: reviews,
+        isLoading: isLoadingReviews,
+        isError: isErrorReviews,
+    } = useMovieReviewQuery(id);
 
     const [expandedReviewId, setExpandedReviewId] = useState(null);
     const [showModal, setShowModal] = useState(false);
+    const [youtubeVideoKey, setYoutubeVideoKey] = useState("");
+    const API_KEY = process.env.REACT_APP_API_KEY;
+
+    useEffect(() => {
+        const fetchYoutubeVideo = async () => {
+            try {
+                const response = await axios.get(
+                    `https://api.themoviedb.org/3/movie/${id}/videos`,
+                    {
+                        headers: {
+                            Accept: "application/json",
+                            Authorization: `Bearer ${API_KEY}`,
+                        },
+                    }
+                );
+
+                const video = response.data.results.find(
+                    (video) =>
+                        video.site === "YouTube" && video.type === "Trailer"
+                );
+                if (video) {
+                    setYoutubeVideoKey(video.key);
+                }
+            } catch (error) {
+                console.error("Failed to fetch YouTube video", error);
+            }
+        };
+
+        fetchYoutubeVideo();
+    }, [id, API_KEY]);
 
     const showGener = (genreIdList) => {
         if (!genreData) return [];
         return genreIdList.map((id) => {
             const genreObj = genreData.find((genre) => genre.id === id);
-            return genreObj ? genreObj.name : '';
+            return genreObj ? genreObj.name : "";
         });
     };
 
     const toggleReview = (reviewId) => {
-        setExpandedReviewId(prevId => prevId === reviewId ? null : reviewId);
+        setExpandedReviewId((prevId) =>
+            prevId === reviewId ? null : reviewId
+        );
     };
 
     const handleImageClick = () => {
@@ -57,8 +102,7 @@ const MovieDetailPage = () => {
                         <div
                             className="movie-detail-img"
                             style={{
-                                backgroundImage:
-                                    `url(https://media.themoviedb.org/t/p/original${data.backdrop_path})`,
+                                backgroundImage: `url(https://media.themoviedb.org/t/p/original${data.backdrop_path})`,
                             }}
                             onClick={handleImageClick} // Handle image click to open modal
                         ></div>
@@ -70,15 +114,26 @@ const MovieDetailPage = () => {
                             </p>
                             <p className="d-d">
                                 <b>Genre:</b>
-                                {showGener(data.genres.map((genre) => genre.id)).map((genre, index) => (
-                                    <div className="movie-detail-genre" key={index}>
+                                {showGener(
+                                    data.genres.map((genre) => genre.id)
+                                ).map((genre, index) => (
+                                    <div
+                                        className="movie-detail-genre"
+                                        key={index}
+                                    >
                                         {genre}
                                     </div>
                                 ))}
                             </p>
-                            <p className="d-d"><b>runtime: </b>{data.runtime}</p>
+                            <p className="d-d">
+                                <b>runtime: </b>
+                                {data.runtime}
+                            </p>
                             <div className="cart-text">
-                                <FontAwesomeIcon icon={faStar} className="star-icon" />
+                                <FontAwesomeIcon
+                                    icon={faStar}
+                                    className="star-icon"
+                                />
                                 <div className="card-vote-c">
                                     {data.vote_average}
                                 </div>
@@ -104,12 +159,25 @@ const MovieDetailPage = () => {
                             reviews.map((review) => (
                                 <div key={review.id} className="review">
                                     <h3>{review.author}</h3>
-                                    <p className={`review-content ${expandedReviewId === review.id ? 'expanded' : 'collapsed'}`}>
+                                    <p
+                                        className={`review-content ${
+                                            expandedReviewId === review.id
+                                                ? "expanded"
+                                                : "collapsed"
+                                        }`}
+                                    >
                                         {review.content}
                                     </p>
                                     {review.content.length > 300 && (
-                                        <button onClick={() => toggleReview(review.id)} className="review-toggle-btn">
-                                            {expandedReviewId === review.id ? "줄이기" : "더보기"}
+                                        <button
+                                            onClick={() =>
+                                                toggleReview(review.id)
+                                            }
+                                            className="review-toggle-btn"
+                                        >
+                                            {expandedReviewId === review.id
+                                                ? "줄이기"
+                                                : "더보기"}
                                         </button>
                                     )}
                                 </div>
@@ -127,15 +195,19 @@ const MovieDetailPage = () => {
                     <Modal.Title>{data.original_title}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <iframe
-                        width="100%"
-                        height="315"
-                        src={`https://www.youtube.com/embed/${data.youtube_video_key}`} // Ensure `youtube_video_key` is available in movie data
-                        title="YouTube video player"
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                    ></iframe>
+                    {youtubeVideoKey ? (
+                        <iframe
+                            width="100%"
+                            height="315"
+                            src={`https://www.youtube.com/embed/${youtubeVideoKey}`}
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    ) : (
+                        <p>No video available</p>
+                    )}
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleCloseModal}>
